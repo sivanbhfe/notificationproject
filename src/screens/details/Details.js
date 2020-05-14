9ad4bb347718c64062435ff7303ba47e621d3aba
Siva <sivansbhfe@gmail.com>
2020-05-15 00:58:32 +0530

    Improved html template

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -13,7 +13,14 @@ class Details extends Component {
 		title:"",
 		platform:"",
 		jiraticket:"",
-		htmlData:[]
+		snticket:"",
+		webstarticket:"",
+		status:"",
+		htmlData:[],
+		timewindow:"",
+		summary:"",
+		incidentmanageremail:"",
+		incidentmanager:""
 		}
 	//	this.seePreview = this.seePreview.bind(this);
 	}
@@ -26,6 +33,24 @@ class Details extends Component {
 	}
 	jiraticketupdate = (e)=> {
 		this.setState({ jiraticket: e.target.value })
+	}
+	snticketupdate = (e)=> {
+		this.setState({ snticket: e.target.value })
+	}
+	webstarticketupdate = (e)=> {
+		this.setState({ webstarticket: e.target.value })
+	}
+
+	statusupdate = (e)=> {
+		this.setState({ status: e.target.value })
+	}
+
+	incidentmanagerupdate = (e)=> {
+		this.setState({ incidentmanager: e.target.value })
+	}
+
+	incidentmanageremailupdate = (e)=> {
+		this.setState({ incidentmanageremail: e.target.value })
 	}
 	seePreview=()=>{	
 		alert("test");	
@@ -58,21 +83,31 @@ class Details extends Component {
 				<FormControl  >
 					<InputLabel htmlFor="platform">Platform</InputLabel>
 					<Input className="inputtitle" onChange={this.platformUpdate} id="platform" />
-				</FormControl>
+				</FormControl><br/>
 				<FormControl  >
 					<InputLabel htmlFor="jiraticket">JIRA</InputLabel>
-					<Input className="jiraticket" onChange={this.jiraticketupdate} id="platform" />
+					<Input className="inputfields" onChange={this.jiraticketupdate} id="jiraticket" />
+				</FormControl>
+				<FormControl  >
+					<InputLabel htmlFor="snticket">ServiceNow</InputLabel>
+					<Input className="inputfields" onChange={this.snticketupdate} id="snticket" />
+				</FormControl>
+				<FormControl  >
+					<InputLabel htmlFor="webstartikcet">Webstar</InputLabel>
+					<Input className="inputfields" onChange={this.webstarticketupdate}  id="webstartikcet" />
 				</FormControl>
 				<FormControl>
-					<InputLabel htmlFor="status">Status</InputLabel>
-					<Input
-						id="status"
-						value=""
+					<InputLabel  htmlFor="status">Status</InputLabel>
+					<Input className="inputfields" id="status" onChange={this.statusupdate}	
 					/>
 				</FormControl>
 				<FormControl >
-					<InputLabel htmlFor="incident-manager">Incident Manager</InputLabel>
-					<Input id="incident-manager" value="" />
+					<InputLabel htmlFor="incident-manager-name">Incident Manager</InputLabel>
+					<Input className="inputfields" id="incident-manager-name"  onChange={this.incidentmanagerupdate}/>
+					</FormControl><br/><br/>
+					<FormControl >
+					<InputLabel htmlFor="incident-manager-email" >Incident Manager Email</InputLabel>
+					<Input className="inputfields" id="incident-manager-email" onChange={this.incidentmanageremailupdate}  />
 				</FormControl><br/><br/>
 				<Button onClick={this.seePreview} style={{ fontWeight:"bold", fontSize: "100%", height:"50px" }} variant="contained" color="default" >
 					Create Preview</Button>
@@ -84,9 +119,42 @@ class Details extends Component {
     <col style={{width:"250px"}}/>
     <col style={{width:"600px"}}/>
   </colgroup><tbody>
-		<tr id="tabletitle"><td colSpan="2">{this.state.title}</td></tr>
-	<tr id="tableticket"><td colSpan="2"><span>Ticket information:</span><span>JIRA: {this.state.jiraticket}</span></td></tr>
-		<tr  ><td className="rowname" >Platform</td><td>{this.state.platform}</td></tr>
+		<tr id="tabletitle" style={{fontSize: "50px", fontFamily: "calibri", color:"rgb(102, 148, 102)", 
+			textAlign: "center", height: "100px"}}>
+				<td colSpan="2">{this.state.title}</td>
+				</tr>
+	<tr id="tableticket">
+		<td colSpan="2"><span>Ticket information: </span>
+		<span>{this.state.jiraticket!=="" ? <span>JIRA: <a href={this.state.jiraticket}>{this.state.jiraticket}</a> </span>:<span></span>}
+		</span>
+		<span>{this.state.snticket!=="" ? <span>ServiceNow: <a href={this.state.snticket}>{this.state.snticket}</a> </span>:<span></span>}
+		</span>
+		<span>{this.state.webstarticket!=="" ? <span>Webstar: <a href={this.state.webstarticket}>{this.state.webstarticket}</a></span>:<span></span>}
+		</span></td>
+		</tr>
+		<tr><td style={{textAlign:"right",fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}} 
+		id="tableplatform" >Platform</td>
+		<td>{this.state.platform}</td></tr>
+		<tr><td style={{textAlign:"right",fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}} 
+		id="tablestatus" >Status</td>
+		<td>{this.state.status}</td></tr>
+		<tr><td style={{textAlign:"right",fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}} 
+		id="tabletime" >Start time & End time</td>
+		<td>{this.state.timewindow}</td></tr>
+		{this.state.incidentmanager!=="" ?
+		<tr><td style={{textAlign:"right",fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}} 
+		id="tabletime" >Incident Manager</td>
+		<td><a href={this.state.incidentmanageremail}> {this.state.incidentmanager}</a></td></tr> : ""
+		}
+		<tr><td style={{textAlign:"right",fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}} 
+		id="tablesummary" >Summary</td>
+		<td>{this.state.summary}</td></tr>
+		<tr><td colSpan="2" style={{fontFamily: "calibri",fontSize:"20px"}}>Please contact  GTO APAC Content Support via reply to this 
+			<a href="mailto:sivabalan.shanmugasiva@lexisnexis.com">email</a> if you have any queries/concerns.</td></tr>
+		<tr><td colSpan="2" style={{fontFamily: "calibri",fontWeight:"bold", fontSize:"20px"}}>Regards<br/>GTO APAC Content Support<br/>
+		Email: <a href="mailto:sivabalan.shanmugasiva@lexisnexis.com">CSS (LNG - AUS)</a><br/>
+		<span style={{fontSize:"15px"}}>Note: Internal & External</span>
+		</td></tr>
 		</tbody>
 						</table>
 					</div>
